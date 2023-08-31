a5ccfb97ad5e0555291497cf09ee01b535c736c6
Breeze <echobydq@gmail.com>
2023-08-31 18:09:22 +0800

    update

--- inc/url_conver.h
+++ inc/url_conver.h
@@ -28,6 +28,6 @@ int hexit(char c);
 
 void strencode(char* to, size_t tosize, const char* from);
 
-void calculate_folder_size(dirname, &total_size); 
+void calculate_folder_size(const char *folder_path, unsigned long long *total_size) 
 
 #endif
